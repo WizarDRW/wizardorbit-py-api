789d6bc3bb8ac26f21ac4d9af89fa48fe1251425
Burak <wizard1456@hotmail.com>
2021-08-02 19:50:23 +0300

    global warning started

---
+++ app/controllers/globalwarning.py
@@ -0,0 +1,17 @@
+
+
+class GlobalWarning:
+    def __init__(self) -> None:
+        pass
+
+    def co2(self):
+        pass
+
+    def nitrousOxide(self):
+        pass
+
+    def methane(self):
+        pass
+    
+    def arctic(self):
+        pass
