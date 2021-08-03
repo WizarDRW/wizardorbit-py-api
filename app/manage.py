ca6cfcc4ab3f8ec3bcf99280b034282910edfa13
Burak <wizard1456@hotmail.com>
2021-08-03 20:16:18 +0300

    manager.py changed

--- app/manage.py
+++ app/manage.py
@@ -1,14 +1,33 @@
-from flask import Flask
-from flask_restful import reqparse, abort, Api, Resource
-import main.routers.globalwarning
-# from controllers.note import *
+import os
+import unittest
 
-app = Flask(__name__)
-api = Api(app)
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
 
-# Setup the Api resource routing
+from app.main import create_app, db
 
-# api.add_resource(NoteResource, '/notes/', endpoint='notes')
+app = create_app(os.getenv('BOILERPLATE_ENV' or 'dev'))
+
+app.app_context().push()
+
+manager = Manager()
+
+migrate = Migrate()
+
+manager.add_command('db', MigrateCommand)
+
+@manager.command
+def run():
+    app.run()
+
+@manager.command
+def test():
+    """Runs the unit tests."""
+    tests = unittest.TestLoader().discover('app/test', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
